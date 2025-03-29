aaadaaf86597906d47a5da27010872470aa35a85
loydjoii <katrinadubkova@gmail.com>
2025-03-29 21:01:59 +0200

    Pievienot cikla kontroli ar break/continue

---
+++ loop_control.py
@@ -0,0 +1,10 @@
+for num in range(1, 11):  
+    if num == 5:  
+        print("skaitlis 5 tika izlaists")  
+        continue 
+
+    if num == 8:  
+        print("cikls apstājas pie 8")  
+        break
+
+    print(num)
